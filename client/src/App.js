666ec98f4239be3af90af27df44c5d2a4f7b528c
Kyle <kyle.e.cardwell@gmail.com>
2021-08-09 22:23:41 -0600

    started EditMovieForm

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,9 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies}/>
         
           <Switch>
-            <Route path="/movies/edit/:id">
+            <Route path="/movies/edit/:id"
+              render={() => <EditMovieForm />}
+            >
             </Route>
 
             <Route path="/movies/:id">
